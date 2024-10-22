63d8ff2723e0d65326b7f6ec4eb640871b21c14c
ShanksLeBlack <149971596+StephDev-tech@users.noreply.github.com>
2024-10-22 10:48:00 -0400

    Ajout de nouveaux éléments
    Correction sur le responsive, mise en place d'un bouton pour le retour à la première page , et mise en place d'un témoin active pour les éléments de la navigation

---
+++ script.js
@@ -0,0 +1,35 @@
+// Sélectionner tous les liens de la navigation
+const lienNavigation = document.querySelectorAll('.element-navigation');
+
+// Ajouter un écouteur d'événement à chaque lien
+lienNavigation.forEach(lien => {
+  lien.addEventListener('click', function() {
+    // Supprimer la classe 'active' de tous les liens
+    lienNavigation.forEach(nav => nav.classList.remove('active'));
+
+    // Ajouter la classe 'active' au lien cliqué
+    this.classList.add('active');
+  });
+});
+
+// Sélectionner le bouton
+const boutonPremierePage = document.getElementById('premierePage');
+
+// Afficher le bouton après avoir défilé 300px vers le bas
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        boutonPremierePage.style.display = 'block';
+    } else {
+        boutonPremierePage.style.display = 'none';
+    }
+});
+
+// Scroll vers le haut avec un effet fluide
+boutonPremierePage.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
